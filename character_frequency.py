9ac6e756efd7224e9df001bbd4673f9314aed9e2
Toroobong Bassey <basseytoro@gmail.com>
2024-02-10 19:28:14 +0100

    assignment on character frequency

--- character_frequency.py
+++ character_frequency.py
@@ -1,9 +1,14 @@
+def char_frequency(string):
+    char_counts = {}
+    for char in string:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
 
- def char_ character (string):
- counts = {}
-  for char in string:
-    if char in char_counts:
-      char_counts[char] += 1
-    else:
-      char_counts[char] = 1
-  return char_counts
+    return char_counts
+
+
+output = char_frequency("semicolon")
+
+print(output)
